29586836c99912e0107ee444ba7899e7614297d8
danishkapoor <danishk6100@gmail.com>
2018-02-16 17:53:11 +0000

    Initial Commit

---
+++ temperature.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ float i,j;
+printf("Enter the temperature in Celsius: ");
+scanf("%f",&i);
+j=(9/5.0*i)+32;
+printf("\nThe temperature in Farenhiet is: %f",j);
+return 0;
+}
